25ee0945f658801ff27b226780c29a01617b32e5
sparkingbeard <tusharbisen8@gmail.com>
2026-03-22 15:14:18 +0530

    refactor: folder structure change

--- src/middleware/roleMiddleware.js
+++
@@ -1,8 +0,0 @@
-export const roleMiddleware = (allowedRoles) => {
-  return (req, res, next) => {
-    if (!allowedRoles.includes(req.role)) {
-      return res.status(403).json({ message: 'Access denied' });
-    }
-    next();
-  };
-};
